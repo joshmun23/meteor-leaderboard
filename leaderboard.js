c3ab27fe1e69a7da6354bf7ad975b6aa833a9980
Joshua Mun <joshmun23@gmail.com>
2015-09-10 19:45:05 -0400

    add security measures by moving logic into serverside and requiring ids as validation to query/update on server side

--- leaderboard.js
+++ leaderboard.js
@@ -1,11 +1,13 @@
 Players = new Mongo.Collection('players');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('thePlayers'),
   Template.leaderboard.helpers({
     msg: function() {
       return 'active players.'
     },
     player: function() {
+      var currentUserId = Meteor.userId();
       return Players.find({}, {
         sort: {score: -1, name: 1}
       });
@@ -44,33 +46,32 @@ if (Meteor.isClient) {
       Session.set('selectedPlayer', PlayerID);
     },
     'click .add-points': function() {
-      var PlayerID = Session.get('selectedPlayer');
+      var info = {
+        playerId: Session.get('selectedPlayer'),
+        isInc: true
+      };
 
-      Players.update(PlayerID, {
-        $inc: {score: 5}
-      });
-
-      // Players.update(score: player.score + 5);
+      Meteor.call('modifyPlayerScore', info);
     },
     'click .take-points': function() {
-      var PlayerID = Session.get('selectedPlayer');
+      var info = {
+        playerId: Session.get('selectedPlayer'),
+        isInc: false
+      };
 
-      Players.update(PlayerID, {
-        $inc: {score: -5}
-      })
+      Meteor.call('modifyPlayerScore', info)
     }
   });
 
   Template.addPlayerForm.events ({
     'submit form': function(e) {
       e.preventDefault();
-      var playerName = e.target.playerName.value;
-      var playerScore = e.target.playerScore.value || 0;
+      var player = {
+        name: e.target.playerName.value,
+        score: e.target.playerScore.value || 0
+      };
 
-      Players.insert({
-        name: playerName,
-        score: parseInt(playerScore) //need to convert into numeric values
-      });
+      Meteor.call('insertPlayerData', player);
       e.target.playerName.value = '';
       e.target.playerScore.value = '';
     },
@@ -78,19 +79,46 @@ if (Meteor.isClient) {
       var selectedPlayer = Session.get('selectedPlayer');
       var removePlayer = confirm('Are you sure you want to delete this player?');
       if (removePlayer) {
-        Players.remove(selectedPlayer);
+        Meteor.call('removePlayerData', selectedPlayer);
       };
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.publish('thePlayers', function(){
+    var currentUserId = this.userId;
+    return Players.find({createdBy: currentUserId});
+  }),
+  Meteor.startup(function() {
     return Meteor.methods({
-      removeAllPlayers: function(){
+      removeAllPlayers: function() {
         // this function allows us to use Meteor.call('removeAllPlayers')
         // client side (Trusted Code is Server Side)
         return Players.remove({})
+      },
+      removeAllUsers: function() {
+        return Meteor.users.remove({});
+      },
+      insertPlayerData: function(player) {
+        var currentUserId = Meteor.userId();
+        Players.insert({
+          name: player.name,
+          //need to convert into numeric values
+          score: parseInt(player.score),
+          createdBy: currentUserId
+        });
+      },
+      removePlayerData: function(selectedPlayer) {
+        var currentUserId = Meteor.userId();
+        Players.remove({_id: selectedPlayer, createdBy: currentUserId});
+      },
+      modifyPlayerScore: function(info) {
+        var score = info.isInc ? 5 : -5
+        console.log(info)
+        Players.update(info.playerId, {$inc: {
+          score: score
+        }});
       }
     });
   });
